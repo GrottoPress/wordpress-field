e2b6fa9eb1aeab27678fd6642de9cc15316269b2
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-08-14 20:14:46 +0000

    initial commit

---
+++ tests/bootstrap.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package GrottoPress\WordPress\Form
+ * @since 0.1.0
+ *
+ * @author GrottoPress (https://www.grottopress.com)
+ * @author N Atta Kus Adusei (https://twitter.com/akadusei)
+ */
+
+declare ( strict_types = 1 );
+
+/**
+ * Autoloader
+ *
+ * @since 0.1.0
+ */
+require_once \dirname( __DIR__ ) . '/vendor/autoload.php';
+
+$_tests_dir = \getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+/**
+ * Give access to tests_add_filter() function.
+ */
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require \dirname( __DIR__ ) . '/src/field.php';
+}
+\tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+/**
+ * Start up the WP testing environment.
+ */
+require $_tests_dir . '/includes/bootstrap.php';
---
+++ tests/field-test.php
@@ -0,0 +1,166 @@
+<?php
+
+/**
+ * Pagination Tests
+ *
+ * @package GrottoPress\WordPress\Form
+ * @subpackage GrottoPress\WordPress\Form\Tests
+ *
+ * @since 0.1.0
+ *
+ * @author GrottoPress (https://www.grottopress.com)
+ * @author N Atta Kus Adusei (https://twitter.com/akadusei)
+ */
+
+declare ( strict_types = 1 );
+
+namespace GrottoPress\WordPress\Form\Tests;
+
+use GrottoPress\WordPress\Form\Field;
+
+/**
+ * Page test case
+ *
+ * @since 0.1.0
+ */
+class Field_Test extends \WP_UnitTestCase {
+    private $dom;
+
+    public function setUp() {
+        $this->dom = new \DOMDocument();
+
+        \register_post_type( 'tutorial', [
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => [ 'slug' => 'tutorials', 'with_front' => false ],
+            'taxonomy' => [ 'level' ],
+        ] );
+
+        \register_taxonomy( 'level', [ 'tutorial' ], [
+            'rewrite' => [ 'with_front' => false ],
+            'hierarchical' => true,
+        ] );
+
+        \wp_insert_term( 'basic', 'level' );
+
+        $this->page_ids = $this->factory->post->create_many( 3, [
+            'post_type' => 'page',
+            'post_status' => 'publish',
+        ] );
+
+        parent::setUp();
+    }
+
+    public function test_categories_dropdown() {
+        $field = new Field( [
+            'type' => 'wp_dropdown_categories',
+            'layout' => 'block',
+            'callable_args' => [
+                [ 
+                    'show_option_none' => '--',
+                    'echo' => 0,
+                    'selected' => 1,
+                    'name' => 'field-name',
+                    'id' => 'field-id',
+                    'taxonomy' => 'category',
+                ],
+            ],
+        ] );
+
+        $this->dom->loadHTML( $field->render() );
+        $ps = $this->dom->getElementsByTagName( 'p' );
+        $selects = $this->dom->getElementsByTagName( 'select' );
+        $options = $this->dom->getElementsByTagName( 'option' );
+
+        $this->assertCount( 1, $ps );
+        $this->assertCount( 1, $selects );
+        $this->assertTrue( count( $options ) > 0 );
+
+        $this->assertSame( 'field-id', $selects->item( 0 )->attributes->getNamedItem( 'id' )->value );
+        $this->assertSame( 'field-name', $selects->item( 0 )->attributes->getNamedItem( 'name' )->value );
+    }
+
+    public function test_tax_dropdown() {
+        $field = new Field( [
+            'type' => 'wp_dropdown_categories',
+            'layout' => 'block',
+            'callable_args' => [
+                [ 
+                    'show_option_none' => '--',
+                    'echo' => 0,
+                    'selected' => 1,
+                    'name' => 'field-name',
+                    'id' => 'field-id',
+                    'taxonomy' => 'level',
+                ],
+            ],
+        ] );
+
+        $this->dom->loadHTML( $field->render() );
+        $ps = $this->dom->getElementsByTagName( 'p' );
+        $selects = $this->dom->getElementsByTagName( 'select' );
+        $options = $this->dom->getElementsByTagName( 'option' );
+
+        $this->assertCount( 1, $ps );
+        $this->assertCount( 1, $selects );
+        $this->assertTrue( count( $options ) > 0 );
+
+        $this->assertSame( 'field-id', $selects->item( 0 )->attributes->getNamedItem( 'id' )->value );
+        $this->assertSame( 'field-name', $selects->item( 0 )->attributes->getNamedItem( 'name' )->value );
+    }
+
+    public function test_pages_dropdown() {
+        $field = new Field( [
+            'type' => 'wp_dropdown_pages',
+            'layout' => 'block',
+            'callable_args' => [
+                [ 
+                    'show_option_none' => '--',
+                    'echo' => 0,
+                    'selected' => 1,
+                    'name' => 'field-name',
+                    'id' => 'field-id',
+                ],
+            ],
+        ] );
+
+        $this->dom->loadHTML( $field->render() );
+        $ps = $this->dom->getElementsByTagName( 'p' );
+        $selects = $this->dom->getElementsByTagName( 'select' );
+        $options = $this->dom->getElementsByTagName( 'option' );
+
+        $this->assertCount( 1, $ps );
+        $this->assertCount( 1, $selects );
+        $this->assertTrue( count( $options ) > 0 );
+
+        $this->assertSame( 'field-id', $selects->item( 0 )->attributes->getNamedItem( 'id' )->value );
+        $this->assertSame( 'field-name', $selects->item( 0 )->attributes->getNamedItem( 'name' )->value );
+    }
+
+    public function test_file_field_render() {
+        $field = new Field( [
+            'id' => 'field-id',
+            'name' => 'field-name',
+            'type' => 'file',
+            'value' => '23',
+            'meta' => [ 'class' => 'my-class' ],
+        ] );
+
+        $this->dom->loadHTML( $field->render() );
+        $ps = $this->dom->getElementsByTagName( 'p' );
+        $inputs = $this->dom->getElementsByTagName( 'input' );
+        $buttons = $this->dom->getElementsByTagName( 'button' );
+        $scripts = $this->dom->getElementsByTagName( 'script' );
+
+        $this->assertCount( 1, $ps );
+        $this->assertCount( 2, $inputs );
+        $this->assertCount( 2, $buttons );
+        $this->assertCount( 1, $scripts );
+
+        $this->assertSame( 'field-id', $inputs->item( 0 )->attributes->getNamedItem( 'id' )->value );
+        $this->assertSame( 'field-name', $inputs->item( 0 )->attributes->getNamedItem( 'name' )->value );
+        $this->assertSame( 'hidden', $inputs->item( 0 )->attributes->getNamedItem( 'type' )->value );
+        $this->assertSame( '23', $inputs->item( 0 )->attributes->getNamedItem( 'value' )->value );
+        $this->assertSame( 'my-class', $inputs->item( 1 )->attributes->getNamedItem( 'class' )->value );
+    }
+}
